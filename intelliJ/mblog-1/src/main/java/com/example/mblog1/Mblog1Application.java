e7b9c606f7b44ee818db23ab8bd7f7d72a9a81db
welsper53 <welsper53@gmail.com>
2023-04-11 19:20:51 +0900

    23.04.11 리액트 서버 빌드

--- intelliJ/mblog-1/src/main/java/com/example/mblog1/Mblog1Application.java
+++ intelliJ/mblog-1/src/main/java/com/example/mblog1/Mblog1Application.java
@@ -20,7 +20,10 @@ public class Mblog1Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                // 두 포트 모두 서비스를 제공하지만 3000은 동기화 된 서비스 제공이고
+                // 7000번은 build 된 코드까지만 제공한다
+                // 결과적으로 3000번은 개발서버로 7000번 서비스 서버로 사용하면 될 것이다
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:7000");
             }
         };
     }
